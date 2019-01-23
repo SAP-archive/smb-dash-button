0c90e5639db0b8a0c208b48116a6c333c1ce5539
Ralphive <ralphrpo@gmail.com>
2019-01-23 12:37:31 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * This is a sample Lambda function that sends an email on click of a
+ * button. It requires these SES permissions.
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ses:GetIdentityVerificationAttributes",
+                "ses:SendEmail",
+                "ses:VerifyEmailIdentity"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+ *
+ * The following JSON template shows what is sent as the payload:
+{
+    "serialNumber": "GXXXXXXXXXXXXXXXXX",
+    "batteryVoltage": "xxmV",
+    "clickType": "SINGLE" | "DOUBLE" | "LONG"
+}
+ *
+ * A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
+ * "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
+ *
+ * For more documentation, follow the link below.
+ * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
+ */
+
+'use strict';
+
+const B1SL = require('./modules/b1ServiceLayer');
+
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', event);
+
+    switch (event.clickType) {
+        case 'SINGLE':
+        case 'DOUBLE':
+        default:
+            B1SL.PostMessage(callback)
+    }
+};
